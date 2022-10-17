04066bba4374ada38c98aa8a7fca296bb11d960d
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2022-10-17 11:08:50 -0700

    fix: change footer to fixed only if the device size is md and up

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,5 @@
 export default function footer() {
   return (
-    <div className='text-white text-center fixed left-0 bottom-0 w-full pb-6'>Made with &#10084; by <a href="https://github.com/MalekD5" className='text-teal-500 underline underline-offset-2'>MalekD5</a></div>
+    <div className='text-white text-center md:fixed mt-2 md:mt-0 left-0 bottom-0 w-full pb-6'>Made with &#10084; by <a href="https://github.com/MalekD5" className='text-teal-500 underline underline-offset-2'>MalekD5</a></div>
   )
 }
